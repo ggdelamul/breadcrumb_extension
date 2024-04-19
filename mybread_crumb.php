5ff6a59380df54ad5f79e68a8f998d94b2e7ca96
jeremy <projeremylegendre@gmail.com>
2024-04-19 13:06:44 +0200

    add admin menu and style admin menu

--- mybread_crumb.php
+++ mybread_crumb.php
@@ -32,5 +32,38 @@ function add_script(){
 } 
 add_action('init', 'add_script');
 add_shortcode( 'mybreadcrumb', 'breadCrumb');
+
+
+//function qui gèrera l'affichage du contenu du menu
+function showContent ()
+  {
+    echo '<div>';
+    echo '<h1>Breadcrumb</h1>';
+    echo '<h2>Mettre en place la fonction breadCrumb sur le header.php avec le code suivant dans les thème classique</h2>';
+    echo '<code>
+            
+            if(function_exists("breadCrumb")){
+            echo breadCrumb();
+            }
+       
+          </code>';
+    echo '<h2>Utilisez le short code pour les thèmes à base de bloc</h2>';
+    echo '<p>ajoutez le code court dans le modèle header du thème</p>';
+    echo '<code>[mybreadcrumb]</code>';
+    echo '<h2>Ajout du bloc code court à l’en-tête avec l’extension Elementor Header & Footer Builder</h2>';
+    echo '<code>[mybreadcrumb]</code>';
+    echo '</div>';
+  }
+//declaration menu extension 
+function addMenuAdmin()
+{
+  add_menu_page('breadcrumb_extension', 'extension_breadcrumb','manage_options', 'extension_breadcrumb', 'showContent','dashicons-arrow-right', 75);
+}
+add_action('admin_menu', 'addMenuAdmin');
+// Ajout style admin 
+function custom_admin_styles() {
+    wp_enqueue_style('custom-admin-styles', plugin_dir_url(__FILE__) . 'assets/css/admin-styles.css');
+}
+add_action('admin_enqueue_scripts', 'custom_admin_styles');
 ?> 
 
